568a449726162f51823e930da5f2c20626b74116
wacho91 <criskol.71@gmail.com>
2024-12-05 11:07:52 -0500

    Navbar Component Complete

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,26 +2,33 @@ import { CiSearch } from "react-icons/ci"
 import { FaDumbbell } from "react-icons/fa"
 import { MdMenu } from "react-icons/md"
 import { PiShoppingCartThin } from "react-icons/pi"
-
+import { NavbarMenu } from "../../MockData/Data"
+import { useState } from "react"
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <>
       <nav>
-          <div>
+          <div  className="container flex justify-between items-center py-8">
             {/* Logo section */}
-            <div>
+            <div className="text-2xl flex items-center gap-2 font-bold uppercase">
               <FaDumbbell />
               <p>Coders</p>
-              <p>Gym</p>
+              <p className="text-secondary">Gym</p>
             </div>
             {/* Menu section */}
-            <div>
-              <ul>
+            <div className="hidden md:block">
+              <ul className="flex items-center gap-6 text-gray-600">
                 {NavbarMenu.map((item) => {
                   return (
                     <li key={item.id}>
-                      <a href={item.link}>
+                      <a 
+                        href={item.link}
+                        className="inline-block py-1 px-3 hover:text-primary font-semibold"
+                      >
                         {item.title}
                       </a>
                     </li>
@@ -30,23 +37,24 @@ const Navbar = () => {
               </ul>
             </div>
             {/* Icons section */}
-            <div>
-              <button>
+            <div className="flex items-center gap-4">
+              <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
                 <CiSearch />
               </button>
-              <button>
+              <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
                 <PiShoppingCartThin />
               </button>
-              <button>
+              <button className="hover:bg-primary text-primary font-semibold hover:text-white rounded-md border-2 border-primary px-6 py-2 duration-200 hidden md:block">
                 Login
               </button>
             </div>
             {/* Mobile hamburger Menu section */}
-            <div>
-              <MdMenu />
+            <div className="md:hidden" onClick={() => setOpen(!open)}>
+              <MdMenu className="text-4xl" />
             </div>
           </div>
       </nav>
+      {/* Mobile Sidebar section */}
     </>
   )
 }
