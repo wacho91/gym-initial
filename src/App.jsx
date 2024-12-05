f9e4c9f0f355ca5422f20a63ec06638965ecdb5e
wacho91 <criskol.71@gmail.com>
2024-12-05 11:48:53 -0500

    Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import Hero from "./component/Hero/Hero";
 import Navbar from "./component/Navbar/Navbar"
+import BgImage from "./assets/bg.png";
+
+
+const bgStyle = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundAttachment: "fixed",
+};
 
 function App() {
 
   return (
-    <div>
-        <Navbar />
+    <div className="overflow-x-hidden">
+        <div style={bgStyle}>
+          <Navbar />
+          <Hero />
+        </div>
     </div>
   )
 }
