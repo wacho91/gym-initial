88ceb4563213eb7cd2acb23d89fea1fb74665c81
wacho91 <criskol.71@gmail.com>
2024-12-05 10:49:31 -0500

    Navbar Arquitecture Complete

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,53 @@
+import { CiSearch } from "react-icons/ci"
+import { FaDumbbell } from "react-icons/fa"
+import { MdMenu } from "react-icons/md"
+import { PiShoppingCartThin } from "react-icons/pi"
 
 
 const Navbar = () => {
   return (
-    <div>
-        Navbar Component
-    </div>
+    <>
+      <nav>
+          <div>
+            {/* Logo section */}
+            <div>
+              <FaDumbbell />
+              <p>Coders</p>
+              <p>Gym</p>
+            </div>
+            {/* Menu section */}
+            <div>
+              <ul>
+                {NavbarMenu.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <a href={item.link}>
+                        {item.title}
+                      </a>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+            {/* Icons section */}
+            <div>
+              <button>
+                <CiSearch />
+              </button>
+              <button>
+                <PiShoppingCartThin />
+              </button>
+              <button>
+                Login
+              </button>
+            </div>
+            {/* Mobile hamburger Menu section */}
+            <div>
+              <MdMenu />
+            </div>
+          </div>
+      </nav>
+    </>
   )
 }
 
